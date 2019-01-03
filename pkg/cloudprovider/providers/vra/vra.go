1c131522c9f7e7ddfd9d2cdf6dbf41fe7daefbda
siddheshwar-more <siddheshwar.more16@gmail.com>
2019-01-03 00:10:40 -0800

    changes for vra provider

---
+++ pkg/cloudprovider/providers/vra/vra.go
@@ -0,0 +1,64 @@
+
+package vra
+
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"reflect"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/mitchellh/mapstructure"
+	"gopkg.in/gcfg.v1"
+
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	netutil "k8s.io/apimachinery/pkg/util/net"
+	certutil "k8s.io/client-go/util/cert"
+	cloudprovider "k8s.io/cloud-provider"
+	v1helper "k8s.io/cloud-provider-vra/pkg/apis/core/v1/helper"
+	"k8s.io/klog"
+)
+
+const (
+	// ProviderName is the name of the vra provider
+	ProviderName = "vra"
+
+)
+
+// ErrNotFound is used to inform that the object is missing
+var ErrNotFound = errors.New("failed to find object")
+
+// ErrMultipleResults is used when we unexpectedly get back multiple results
+var ErrMultipleResults = errors.New("multiple results where only one expected")
+
+// ErrNoAddressFound is used when we cannot find an ip address for the host
+var ErrNoAddressFound = errors.New("no address found for host")
+
+// ErrIPv6SupportDisabled is used when one tries to use IPv6 Addresses when
+// IPv6 support is disabled by config
+var ErrIPv6SupportDisabled = errors.New("IPv6 support is disabled")
+
+// MyDuration is the encoding.TextUnmarshaler interface for time.Duration
+type MyDuration struct {
+	time.Duration
+}
+
+// UnmarshalText is used to convert from text to Duration
+func (d *MyDuration) UnmarshalText(text []byte) error {
+	res, err := time.ParseDuration(string(text))
+	if err != nil {
+		return err
+	}
+	d.Duration = res
+	return nil
+}
+
